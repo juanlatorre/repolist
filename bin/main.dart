422feaab69922d91a96597d88de1a31553699351
Juan Latorre <juanlatorre@protonmail.com>
2020-01-13 22:22:31 -0300

    regex gitrhub username checker

--- bin/main.dart
+++ bin/main.dart
@@ -1,19 +1,22 @@
 import 'package:github/github.dart';
+import 'dart:io';
+import 'package:args/args.dart';
 
-Future<void> main() async {
-    var github = GitHub();
-    // check login
+RegExp githubUserRegExp = RegExp(r'^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$');
+var github = GitHub();
 
+void main(List<String> args) {
+  var parser = ArgParser();
+  parser.addOption('public', abbr: 'p');
+  var results = parser.parse(args);
 
-
-    //var repo = await github.repositories.getRepository(RepositorySlug('juanlatorre', 'repolist'));
-    //print(repo);
+  if (githubUserRegExp.hasMatch(results['public'])) {
+    getPublicRepos(results['public']);
+  } else {
+    stdout.writeln('This is not a GitHub username. Please try again.');
+  }
 }
 
-// check login
-  // if logged in, go to func
-  // if not, ask for login
-
-// func
-  // $ repolist / --my-repolist
-  // $ repolist -p <username> / --public-repolist 
+Future<void> getPublicRepos(String user) async {
+  print(user);
+}
